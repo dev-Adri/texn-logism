d01b9cdbaf84eb6a3ff9fa1f6640f1bb4325b4b8
dev-Adri <adrisoftdev@gmail.com>
2024-05-23 14:32:40 +0300

    Updated the frotned + changed some plots
    Upadted the frontend to look better and more user friendly. Changed some plots to be able to generalize with any data set. Used state variables so the dataset can be loaded from any tab. Added 2 tabs to machine learning algorith. TODO: Add classification and clustering algorithms and run them.

--- app.py
+++ app.py
@@ -3,101 +3,201 @@ import streamlit as st
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
+
 from sklearn.decomposition import PCA
 from sklearn.manifold import TSNE
+from sklearn.impute import SimpleImputer
 
-# Τίτλος
-st.title('Εφαρμογή ...')
+from streamlit_option_menu import option_menu
 
-# Φόρτωση αρχείου
-uploaded_file = st.file_uploader("Επιλέξτε ένα αρχείο CSV ή Excel", type=["csv", "xlsx"])
+from sklearn.preprocessing import OneHotEncoder, LabelEncoder
 
-if uploaded_file is not None:
-    try:
-        # Ανάγνωση του αρχείου με βάση τον τύπο του
-        if uploaded_file.name.endswith('.csv'):
-            df = pd.read_csv(uploaded_file)
+selected = option_menu(
+    menu_title=None,
+    options=["Home", "2D-Visualization", "Machine Learning"],
+    icons=["house", "graph-up", "robot"],
+    orientation="horizontal",
+)
 
-        else:
-            df = pd.read_excel(uploaded_file)
+def preprocess_data(df):
+    df = df.copy()
+    
+    for col in df.columns:
+        if df[col].dtype == 'object':
+            try:
+                df[col] = pd.to_numeric(df[col])
+            except ValueError:
+                try:
+                    le = LabelEncoder()
+                    df[col] = le.fit_transform(df[col])
+                except Exception as e:
+                    df.drop(columns=[col], inplace=True)
+        elif not pd.api.types.is_numeric_dtype(df[col]):
+            df.drop(columns=[col], inplace=True)
+    
+    # Impute NaN values
+    imputer = SimpleImputer(strategy='mean')
+    df = pd.DataFrame(imputer.fit_transform(df), columns=df.columns)
+    
+    return df
 
-        # Εμφάνιση των γραμμών και στηλών
-        st.write("Σύνολο γραμμών (δείγματα):", df.shape[0])
-        st.write("Σύνολο στηλών (χαρακτηριστικά + ετικέτα):", df.shape[1])
+
+def read(uploaded_file):
+    if uploaded_file.name.endswith('.csv'):
+        return pd.read_csv(uploaded_file)
+
+    else:
+        return pd.read_excel(uploaded_file)
+
+
+if "file_pd" not in st.session_state:
+    st.session_state.file_pd = None
+    
+if "preprocessed" not in st.session_state:
+    st.session_state.preprocessed = False
+    
+if "processed_data" not in st.session_state:
+    st.session_state.processed_data = None
+
+if selected == "Home":
+    # Φόρτωση αρχείου
+    uploaded_file = st.file_uploader("Επιλέξτε ένα αρχείο CSV ή Excel", type=["csv", "xlsx"])
+
+    if uploaded_file is not None:
+        try:
+            df = read(uploaded_file)
+
+            # Save file to browser session, so even if page reloads -
+            # - (which it does when you change tab) the file wont be lost
+            st.session_state.file_pd = df
+
+            # Εμφάνιση των γραμμών και στηλών
+            st.text(f"Σύνολο γραμμών (δείγματα): {df.shape[0]}")
+            st.text(f"Σύνολο στηλών (χαρακτηριστικά + ετικέτα): {df.shape[1]}")
+        except Exception as e:
+            st.error(f"Σφάλμα κατά την φόρτωση των δεδομένων: {e}")
+            
+elif selected == "2D-Visualization":
+    if st.session_state.file_pd is None:
+        st.title("You have not uploaded any data.")
+        st.text("Please go back to the home page and upload a CSV or Excel file.")
+    else:
+        st.title("2D Visualization")
+
+        data = st.session_state.file_pd
+
+        ppd, pca, tsne, eda = st.tabs(["Preprocess", "PCA", "t-SNE", "Explore your data"])
         
-        # Έλεγχος ότι υπάρχει τουλάχιστον μία στήλη χαρακτηριστικών και μία στήλη ετικετών
-        if df.shape[1] < 2:
-            st.error("Ο πίνακας πρέπει να περιλαμβάνει τουλάχιστον μία στήλη χαρακτηριστικών και μία στήλη ετικετών.")
-        else:
-            # Διαχωρισμός των χαρακτηριστικών και των ετικετών
-            features = df.iloc[:, :-1]
-            labels = df.iloc[:, -1]
+        pca_data, tsne_data = None, None
 
-            # Χειρισμός τιμών NaN
-            if features.isnull().values.any():
-                st.warning("Τα δεδομένα περιέχουν τιμές NaN. Θα αντικατασταθούν με τη μέση τιμή της κάθε στήλης.")
-                features = features.fillna(features.mean())
-
-            # Χρήση tabs για τα διάγραμματα
-            tab1, tab2 = st.tabs(["Δεδομένα", "2D Οπτικοποιήσεις & EDA"])
-
-            with tab1:
-                st.write("Χαρακτηριστικά (SxF):")
-                st.dataframe(features)
-
-                st.write("Ετικέτες (F+1):")
-                st.dataframe(labels)
-
-            with tab2: 
-                # Σταθερή τιμή perplexity
-                perplexity = min(30, features.shape[0] - 1)
+        with ppd:
+            if st.button("Preprocess Data"):
+                st.session_state.preprocessed = True
                 
-                # Εφαρμογή PCA
-                pca = PCA(n_components=2)
-                pca_result = pca.fit_transform(features)
-                pca_df = pd.DataFrame(pca_result, columns=['PCA1', 'PCA2'])
-                pca_df['label'] = labels.values
+                placeholder = st.empty()
+                placeholder.text("Processing ...")
+                
+                data = preprocess_data(data)
+                st.session_state.processed_data = data
+                
+                placeholder.text(data)
+                
+        with pca:
+            if st.button("Generate PCA graph"):
+                if st.session_state.preprocessed == False:
+                    st.error("Please preprocess the data first.")
+                else:
+                    placeholder = st.empty()
+                    placeholder.text("Generating PCA graph...")
+                    
+                    pca = PCA(n_components=2)
+                    pca.fit(st.session_state.processed_data)
+                    pca_data = pca.transform(st.session_state.processed_data)
+                    
+                    plt.figure(figsize=(10, 6))
+                    plt.scatter(pca_data[:, 0], pca_data[:, 1])
+                    plt.xlabel('X')
+                    plt.ylabel('Y')
+                    plt.title('PCA')
+                    
+                    st.pyplot(plt.gcf())
+                    placeholder.empty()
+                
+        with tsne:
+            if st.button("Generate t-SNE graph"):
+                if st.session_state.preprocessed == False:
+                    st.error("Please preprocess the data first.")
+                else:
+                    placeholder = st.empty()
+                    placeholder.text("Generating t-SNE graph...")
+                    
+                    tsne = TSNE(n_components=2, perplexity=30)
+                    tsne_data = tsne.fit_transform(st.session_state.processed_data)
+                    
+                    plt.figure(figsize=(10, 6))
+                    plt.scatter(tsne_data[:, 0], tsne_data[:, 1])
+                    plt.xlabel('X')
+                    plt.ylabel('Y')
+                    plt.title('t-SNE')
+                    
+                    st.pyplot(plt.gcf())
+                    placeholder.empty()
+        
+        with eda:
+            # st.session_state.processed_data -> Proccesed data (used for PCA and t-SNE)
+            # st.session_state.file_pd -> Unprocessed data
+            if st.button("Explore your data"):
+                if st.session_state.preprocessed == False:
+                    st.error("Please preprocess the data first.")
+                else:
+                    if st.session_state.processed_data.shape[1] < 2:
+                        st.write("Cannot run exploratory data analysis on a dataset with less than 2 features.")
+                    else:
+                        df = st.session_state.processed_data
+                        placeholder = st.empty()
+                        
+                        placeholder.text("Generating a scatter plot ...")
+                        # Scatter plot (separate figure)
+                        if len(df.columns) >= 2:
+                            plt.figure(figsize=(10, 6))
+                            sns.scatterplot(x=df.columns[0], y=df.columns[1], data=df)
+                            plt.title(f'Scatter Plot of {df.columns[0]} and {df.columns[1]}')
+                            plt.xlabel(df.columns[0])
+                            plt.ylabel(df.columns[1])
+                            st.pyplot(plt.gcf())  # Display the scatter plot
+                        
+                        placeholder.text("Generating a distribution plot ...")
+                        # Distribution plot (separate figure)
+                        if len(df.columns) >= 1:
+                            plt.figure(figsize=(10, 6))
+                            sns.histplot(df[df.columns[0]], kde=True, color='blue')
+                            plt.title(f'Distribution of {df.columns[0]}')
+                            plt.xlabel(df.columns[0])
+                            plt.ylabel('Frequency')
+                            st.pyplot(plt.gcf())  # Display the distribution plot
 
-                st.write("PCA Διάγραμμα:")
-                fig, ax = plt.subplots()
-                sns.scatterplot(x='PCA1', y='PCA2', hue='label', data=pca_df, ax=ax)
-                st.pyplot(fig)
+                        placeholder.text("Generating a pair plot for all features (might take some time) ...")
+                        # Create a separate figure for the pair plot
+                        pairplot_fig = sns.pairplot(df)
+                        pairplot_fig.figure.suptitle('Pair Plot of the Dataset')
+                        st.pyplot(pairplot_fig)
+                        
+                        placeholder.empty()
 
-                # Εφαρμογή t-SNE
-                tsne = TSNE(n_components=2, perplexity=perplexity, random_state=42)
-                tsne_result = tsne.fit_transform(features)
-                tsne_df = pd.DataFrame(tsne_result, columns=['t-SNE1', 't-SNE2'])
-                tsne_df['label'] = labels.values
+elif selected == "Machine Learning":
+    if st.session_state.file_pd is None:
+        st.title("You have not uploaded any data.")
+        st.text("Please go back to the home page and upload a CSV or Excel file.")
+    else:
+        if st.session_state.preprocessed == False:
+            st.error("Please preprocess the data first.")
+        else:
+            st.title("Machine learning")
 
-                st.write("t-SNE Διάγραμμα:")
-                fig, ax = plt.subplots()
-                sns.scatterplot(x='t-SNE1', y='t-SNE2', hue='label', data=tsne_df, ax=ax)
-                st.pyplot(fig)
-
-
-                # Exploratory Data Analysis (EDA) Διαγράμματα
-                st.write("EDA Διαγράμματα:")
-
-                # Διάγραμμα κατανομής (Histogram)
-                st.write("Διάγραμμα Κατανομής για την πρώτη στήλη χαρακτηριστικών:")
-                fig, ax = plt.subplots()
-                sns.histplot(features.iloc[:, 0], kde=True, ax=ax)
-                st.pyplot(fig)
-
-                # Boxplot για τον εντοπισμό outliers
-                st.write("Boxplot για την πρώτη στήλη χαρακτηριστικών:")
-                fig, ax = plt.subplots()
-                sns.boxplot(y=features.iloc[:, 0], ax=ax)
-                st.pyplot(fig)
-
-                # Pairplot για να δούμε τη σχέση μεταξύ χαρακτηριστικών
-                st.write("Pairplot για τα πρώτα 3 χαρακτηριστικά:")
-                if features.shape[1] >= 3:
-                    fig = sns.pairplot(features.iloc[:, :3])
-                    st.pyplot(fig)
-
-    except Exception as e:
-        st.error(f"Σφάλμα κατά την φόρτωση των δεδομένων: {e}")
-
-else:
-    st.info("Παρακαλώ φορτώστε ένα αρχείο για να ξεκινήσετε.")
+            classification, clustering = st.tabs(["Classification algorithms", "Clustering algorithms"])
+            
+            with classification:
+                st.write("Edw tha valoume ton xristi na epilegei kapies parametrous ...")
+                
+            with clustering:
+                st.write("Edw tha valoume ton xristi na epilegei kapies parametrous 2 ...")
